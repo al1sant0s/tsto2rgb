edeb756a4a375b77846be0b7249acae9cf2a8ac7
Alisson Santos <cnctalissonsantos@gmail.com>
2025-01-29 01:16:51 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,93 @@
+# Convert image files to RGB files for TSTO
+
+This package allows you to convert images into RGB assets for the 'The Simpsons: Tapped Out' game.
+It uses [**ImageMagick**](https://imagemagick.org/) to perform the conversions.
+
+## Installation
+
+First, make sure you install [**ImageMagick**](https://imagemagick.org/script/download.php) in your system.
+You'll also need to install [**python**](https://www.python.org/downloads/)
+and [**git**](https://git-scm.com/downloads) if you don't already have them installed in your system.
+
+With everything ready, run either of the following commands in the command-line interface, according to your OS:
+
+* Windows installation command.
+```
+python -m pip install tsto2rgb@git+https://github.com/al1sant0s/tsto2rgb
+```
+* Linux installation command.
+```
+python3 -m pip install tsto2rgb@git+https://github.com/al1sant0s/tsto2rgb
+```
+
+## Usage
+
+```
+tsto2rgb --help
+```
+
+The convert tool will receive a list of directories to search for the image files of specified format, convert them into rgb files and then
+it will save the results in the last directory you provide. For example, supposing the image files are inside a directory called 'img_dir' and you want the images to be exported to the 'destination' directory, you would issue the following command:
+
+```
+tsto2rgb path/to/img_dir path/to/destination
+```
+
+The rgb files will be saved in subdirectories within the destination directory.
+
+## Arguments
+
+If you prefer to use a different type of image instead of png, you can use the --input_extension argument.
+
+```
+tsto2rgb --input_extension webp path/to/img_dir path/to/destination
+```
+
+You can also choose the depth of the image. Choose between 4 (default) or 8 bits per channel.
+
+```
+tsto2rgb --depth 8 path/to/img_dir path/to/destination
+```
+
+## Multiple directories
+
+An example specifying multiple directories as input and saving the results in the assets directory.
+
+```
+tsto2rgb path/to/Downloads/img_files path/to/Images/img_folder path/to/Images/another_img_folder path/to/assets
+```
+
+If you want to search for the image files recursively in every subdirectory bellow a specific directory, give the root directory as input. The following example will convert all the image files within the 'Images' directory,
+including the 'img_folder' and 'another_img_folder' subdirectories.
+
+```
+tsto2rgb path/to/Downloads/img_files path/to/Images path/to/assets
+```
+
+## Grouping images
+
+Set the --group argument to organize the images in subdirectories within the destination directory.
+Each subdirectory corresponds to a certain entity. For example, if there were two image files in 'img_dir', one named
+'yellowhouse.png' and the other named 'orangehouse.png', the destination directory would have the following structure after the conversion:
+
+- destination/
+  - destination/yellowhouse/
+  - destination/orangehouse/
+
+Sometimes an entity also has variations. Suppose for example there exists 'yellowhouse_normal.png' and 'yellowhouse_premium.png'. Then this would be the resulting directory structure:
+
+- destination/
+  - destination/yellowhouse
+    - destination/yellowhouse/normal
+    - destination/yellowhouse/premium
+  - destination/orangehouse
+    - destination/orangehouse/_default
+
+In theory, the entity is just the name that precedes the first underscore character in a filename and the variation
+is the rest after the underscore excluding the image extension.
+For example, given the name 'something_anything_else.png', _something_ is the entity and _anything_else_ is the variation.
+When a file corresponding to an entity doesn't specify a variation (a filename without an underscore in it like 'orangehouse.png') the _default variation subdirectory will be created.
+
+```
+tsto2rgb --group path/to/Images path/to/assets
+```
---
+++ src/tsto2rgb/tools/convert.py
@@ -0,0 +1,164 @@
+import argparse
+from pathlib import Path
+from wand.image import Image
+from tsto2rgb.tools.progress import report_progress
+
+
+def progress_str(n, total, filestem, extension):
+    return f"Progress ({n * 100 / total:.2f}%) : [{total - n} file(s) left] ---> {filestem}.{extension}"
+
+
+# Warning: this script requires ImageMagick to work. If you do not have installed in your system,
+# you will have to install it first before using the script.
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="""
+        This tool allows you to convert images back to the raw RGB file format used in the game 'The Simpsons: Tapped Out'.
+        It uses ImageMagick to perform the conversions, so make sure you have it installed in your system.
+        Multiple options are available for customizing the results. You can choose the file extension of the input images, where to save it, etc.
+        Check the help for more information.
+        """,
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+
+    parser.add_argument(
+        "--group",
+        help="If this option is enabled, the files will be separated into subfolders within the output directory.",
+        action="store_true",
+    )
+
+    parser.add_argument(
+        "--depth",
+        help="Depth used for the image (choose between 4 and 8 bits per channel).",
+        default=4,
+        type=int,
+    )
+
+    parser.add_argument(
+        "--input_extension",
+        help="""
+        Image format used for the imported images.
+        You can choose between most of ImageMagick supported image formats like png, webp, jpg, etc.
+        Check ImageMagick documentation for more info about the image formats you can use.
+        """,
+        default="png",
+    )
+
+    parser.add_argument(
+        "input_dir",
+        help="List of directories containing the image files.",
+        nargs="+",
+    )
+
+    parser.add_argument(
+        "output_dir",
+        help="Directory where results will be stored.",
+    )
+
+    args = parser.parse_args()
+    directories = [Path(item) for item in args.input_dir]
+
+    # Help with the progress report.
+    n = 0
+    total = 0
+
+    print(f"\n\n--- CONVERTING {args.input_extension.upper()} FILES TO RGB FILES ---\n\n")
+
+    print(
+        "Counting the number of files to convert (this might take a while) - ", end=""
+    )
+
+    # Get total of files to convert and extract zipped files.
+
+    total = sum(
+        [len(list(Path(directory).glob(f"**/*.{args.input_extension}"))) for directory in directories]
+    )
+
+    if total == 0:
+        raise Exception(f"No {args.input_extension} files found in the specified directories.")
+
+    print(f"[{total} file(s) found!]\n\n")
+
+    print(f"--- Starting conversion of {args.input_extension} images ---")
+
+    # Set destination of the converted rgb files.
+    target = Path(args.output_dir)
+    target.mkdir(exist_ok=True)
+
+    for directory in directories:
+
+        # Process the remaining image files.
+        for img_file in directory.glob(f"**/*.{args.input_extension}"):
+            n += 1
+            report_progress(progress_str(n, total, img_file.stem, args.input_extension), "")
+
+            if args.group is True:
+                entity = img_file.stem.split("_", maxsplit=1)
+
+                if len(entity) == 2:
+                    target = Path(
+                        args.output_dir, entity[0], entity[1].split("_image", maxsplit=1)[0]
+                    )
+                else:
+                    target = Path(args.output_dir, entity[0], "_default")
+
+            else:
+                target = Path(args.output_dir)
+
+            target.mkdir(parents=True, exist_ok=True)
+
+            rgb_file = Path(target, f"{img_file.stem}.rgb")
+
+            if args.depth == 4:
+                with Image(filename=img_file).fx("u * a", channel="rgb") as tmp_img:
+                    width = tmp_img.width
+                    height = tmp_img.height
+
+                    # Swap collor channels for 4 bit depth.
+                    with Image() as rgb_img:
+                        rgb_img.image_add(tmp_img.channel_images["blue"])   # type: ignore
+                        rgb_img.image_add(tmp_img.channel_images["alpha"])  # type: ignore
+                        rgb_img.image_add(tmp_img.channel_images["red"])    # type: ignore
+                        rgb_img.image_add(tmp_img.channel_images["green"])  # type: ignore
+                        rgb_img.combine(colorspace="srgb")
+                        rgb_img.depth = args.depth
+                        rgb_img.format = "rgba"
+
+                        with open(rgb_file, "wb") as binary_file:
+                            # Append signature and image dimensions.
+                            binary_file.write(int.to_bytes(0, 2, "little"))
+                            binary_file.write(int.to_bytes(8192, 2, "little"))
+                            binary_file.write(int.to_bytes(width, 2, "little"))
+                            binary_file.write(int.to_bytes(height, 2, "little"))
+
+                            # Append rest of the image.
+                            binary_file.write(rgb_img.make_blob())  # type: ignore
+
+            elif args.depth == 8:
+                with Image(filename=img_file) as rgb_img:
+                    width = rgb_img.width
+                    height = rgb_img.height
+
+                    rgb_img.depth = args.depth
+                    rgb_img.format = "rgba"
+
+                    with open(rgb_file, "wb") as binary_file:
+                        # Append signature and image dimensions.
+                        binary_file.write(int.to_bytes(0, 2, "little"))
+                        binary_file.write(int.to_bytes(0, 2, "little"))
+                        binary_file.write(int.to_bytes(width, 2, "little"))
+                        binary_file.write(int.to_bytes(height, 2, "little"))
+
+                        # Append rest of the image.
+                        binary_file.write(rgb_img.make_blob())  # type: ignore
+            else:
+                # Ignore this file if it cannot be parsed.
+                # Clear line.
+                print(150 * " ", end="\r")
+                print(f"Invalid {args.input_extension} file. Skipping this file -> {img_file.name}!")
+                continue
+
+
+    print("\n\n--- JOB COMPLETED!!! ---\n\n")
